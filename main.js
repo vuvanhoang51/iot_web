670f15df7c2328bad879cf5b7bd8b68a575b25c5
vuvanhoang51 <69500421+vuvanhoang51@users.noreply.github.com>
2022-05-12 14:45:10 +0700

    Update main.js

--- main.js
+++ main.js
@@ -231,7 +231,7 @@ userData.on('value', snap => {
         });
         myChart.update();
     };
-    setInterval(addData,35000);
+    setInterval(addData,40000);
 
     //==============================================================================================
 
